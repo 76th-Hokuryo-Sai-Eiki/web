a2569a3b06d096099f23a3472fad0dd0a9f45ab8
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-07-07 22:51:35 +0900

    format, modify vite config

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.GITHUB_PAGES ? "/web/" : "/"}>
             <Provider>
                 <App />
             </Provider>
