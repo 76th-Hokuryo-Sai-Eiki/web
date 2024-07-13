ff9af9f64cf45084a2ee76a2e9cad418eadf2544
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-07-13 19:28:19 +0900

    use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
@@ -8,10 +8,10 @@ import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Provider>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
