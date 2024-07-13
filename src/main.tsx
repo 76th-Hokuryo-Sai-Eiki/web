29a6379acffefb8c960addcbc96dfc0fd8c7dd8e
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-07-13 20:36:52 +0900

    lock

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { ReactNode, StrictMode, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,11 +6,36 @@ import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
 
+import { sha256 } from "js-sha256";
+
+let asked = false;
+function Lock({ children }: { children: ReactNode }) {
+    const HASH = import.meta.env.VITE_PASS_HASH;
+    if (!HASH) return children;
+
+    const [hash, setHash] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!asked && hash === null) {
+            asked = true;
+            setHash(sha256(window.prompt("Pass?") ?? ""));
+        }
+    }, []);
+
+    return hash === HASH ? (
+        children
+    ) : (
+        <h1 style={{ fontSize: "28pt" }}>Forbidden</h1>
+    );
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter basename={"/web/"}>
             <Provider>
-                <App />
+                <Lock>
+                    <App />
+                </Lock>
             </Provider>
         </BrowserRouter>
     </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     base: "/web/",
-    plugins: [react(), tsconfigPaths()],
-});
+    plugins: [react(), tsconfigPaths(), htmlPlugin(loadEnv(mode, "."))],
+}));
+
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+    return {
+        name: "html-transform",
+        transformIndexHtml: {
+            enforce: "pre" as const,
+            transform: (html: string): string =>
+                html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+        },
+    };
+}
