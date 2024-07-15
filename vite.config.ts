758fb2d306bd7510ba490eb86eb51df649a132df
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-07-16 06:02:23 +0900

    modify config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: process.env.VITE_BASE,
-    plugins: [react(), tsconfigPaths(), htmlPlugin(loadEnv(mode, "."))],
-}));
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, ".");
+
+    return ({
+        base: env.VITE_BASE,
+        plugins: [react(), tsconfigPaths(), htmlPlugin(env)],
+    })
+});
 
 function htmlPlugin(env: ReturnType<typeof loadEnv>) {
     return {
