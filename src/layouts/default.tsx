5cba3d22f50f995d1e0cc192369a110fb20e61d3
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-07-16 15:18:14 +0900

    Update default.tsx

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -6,9 +6,14 @@ export default function DefaultLayout({ children }: { children: ReactNode }) {
     return (
         <div className="relative flex flex-col h-fit">
             <Navbar />
+
             <main className="container mx-auto px-6 flex-grow pt-16">
                 {children}
             </main>
+
+            <footer className="mt-6" id="footer">
+                <Footer />
+            </footer>
         </div>
     );
 }
