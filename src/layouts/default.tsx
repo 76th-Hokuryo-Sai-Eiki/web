8ab0546a111d828fa11d44e039c8602ca1b0b946
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-07-16 15:20:02 +0900

    add imports

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 
 import { Navbar } from "@/components/navbar";
+import Footer from "@/pages/footer";
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
     return (
